398d04ed70ac9ff48179bb74b8fe02dfd7118159
Eshiryu <m2l4x1@gmail.com>
2026-06-17 16:03:44 +0200

    Anims finies - correction rig
    Les timelines sont finies, je suis en train d'essayer de corriger certaines erreurs du rig auto quand il y a meshs qui clip pendant certaines anims

--- Assets/Perso/Set Up/Amel/FixRigAmel.cs
+++ Assets/Perso/Set Up/Amel/FixRigAmel.cs
@@ -8,11 +8,13 @@ public Rig FixRig;
 public void IK_On()
 {
     FixRig.weight = 1f;
-}
+    Debug.Log("IK ON");
+    }
 
 public void IK_Off()
 {
     FixRig.weight = 0f;
-}
+        Debug.Log("IK OFF");
+    }
     
 }
